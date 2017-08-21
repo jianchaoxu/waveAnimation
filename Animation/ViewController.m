be207cbd0c83aced93cd93a69d05820886771c53
apple <apple@appledeMac-mini.local>
2017-08-21 17:13:39 +0800

    水纹动画

--- Animation/ViewController.m
+++ Animation/ViewController.m
@@ -7,18 +7,37 @@
 //
 
 #import "ViewController.h"
+#import "RippleView.h"
+#import "RippleModel.h"
+
 
 @interface ViewController ()
-
+{
+    RippleView *rippleview;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    EAGLContext *context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    rippleview = [[RippleView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) context:context];
+    [self.view addSubview:rippleview];
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleGesture:)];
+    [rippleview addGestureRecognizer:pan];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleGesture:)];
+    [rippleview addGestureRecognizer:tap];
 }
 
+- (void)handleGesture:(UITapGestureRecognizer *)gesture
+{
+    CGPoint location = [gesture locationInView:rippleview];
+    [rippleview.ripple initiateRippleAtLocation:location];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
