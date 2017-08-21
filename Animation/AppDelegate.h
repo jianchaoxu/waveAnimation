eb51fc2016683cfb06e1b25f15c9fae185f1041d
apple <apple@appledeMac-mini.local>
2017-08-21 16:02:15 +0800

    Initial Commit

---
+++ Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by apple on 2017/8/21.
+//  Copyright © 2017年 xjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Animation
+//
+//  Created by apple on 2017/8/21.
+//  Copyright © 2017年 xjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
